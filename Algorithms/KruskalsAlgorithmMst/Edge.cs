8c6c399550bf59617d0eb226e48d36e67b9d45ee
Ak Hasanuzzaman <ak.hasanuzzaman@lomosoft.de>
2019-10-22 15:57:54 +0200

    Initial commit

---
+++ Algorithms/KruskalsAlgorithmMst/Edge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KruskalsAlgorithmMst
+{
+    public class Edge
+    {
+        public int Vertex1 { get; set; }
+        public int Vertex2 { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using BreadthFirst;
+using BreadthFirstAlgorithm.Dependents;
+using DisjointSetsDataStructure;
+
+namespace BreadthFirst
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            #region BreadthFirstAlgorithm
+            //var bfs = new BreadthFirstAlgorithm();
+            //Employee root = bfs.BuildEmployeeGraph();
+            //Console.WriteLine("Traverse Graph\n------");
+            //bfs.Traverse(root);
+
+            //Console.WriteLine("\nSearch in Graph\n------");
+            //Employee e = bfs.Search(root, "Eva");
+            //Console.WriteLine(e == null ? "Employee not found" : e.name);
+            //e = bfs.Search(root, "Brian");
+            //Console.WriteLine(e == null ? "Employee not found" : e.name);
+            //e = bfs.Search(root, "Soni");
+            //Console.WriteLine(e == null ? "Employee not found" : e.name);
+            #endregion
+
+
+            #region MyRegion
+            //vertices
+            //int[] vertices = new int[] { 1, 2, 3, 5, 6 };
+            //just a bigger number than highest value of vertices
+
+            int[] vertices = new int[] { 0, 1, 2, 3, 4 };
+
+
+            Set set = new Set(100);
+            foreach (int vertex in vertices)
+            {
+                set.MakeSet(vertex);
+            }
+            //unions
+            //set.Union(2, 3);
+            //set.Union(5, 6);
+            //set.Union(3, 6);
+            //set.Union(6, 1);
+
+            set.Union(1, 0);
+            set.Union(2, 4);
+            set.Union(1, 3);
+            set.Union(3, 0);
+            set.Union(4, 0);
+
+            //printing final set
+            foreach (int vertex in vertices)
+            {
+                Console.WriteLine("parent of {0} is: {1} and rank is: {2}", vertex, set.FindImmidiateParent(vertex), set.FindRank(vertex));
+            }
+
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
